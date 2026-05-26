1a93ddffef5dcf956555e1165188bc0769ca1f94
kingabse192-web <ab@localhost>
2026-05-26 10:55:44 +0300

    Remove loading screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = document.getElementById('root')!;
-const loading = document.getElementById('loading');
-if (loading) {
-  loading.style.opacity = '0';
-  setTimeout(() => loading.remove(), 300);
-}
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
