a80e68a3e3e705a3231e01d65f4a075db23a3e1c
kingabse192-web <ab@localhost>
2026-05-28 13:30:22 +0300

    Add announcement banner & calendar download

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import Navbar from './components/Navbar';
+import AnnouncementBanner from './components/AnnouncementBanner';
 import Footer from './components/Footer';
 import ScrollToTopButton from './components/ScrollToTopButton';
 import Home from './pages/Home';
@@ -90,6 +91,7 @@ function App() {
             toggleLang={toggleLang} 
             toggleTheme={toggleTheme} 
           />
+          <AnnouncementBanner lang={lang} />
           
           <main className="flex-grow">
             <AnimatedRoutes lang={lang} />
